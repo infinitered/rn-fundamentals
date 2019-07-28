e1c6881dfc5d4c2ce476b4b475263ffc79391179
fvonhoven <frank@infinite.red>
2019-07-28 10:56:49 -0500

    Make App a React Component

--- App.js
+++ App.js
@@ -8,31 +8,42 @@ import {
   Image,
   Text
 } from "react-native"
-import { camps } from "./fixtures"
+import { CAMPS } from "./fixtures"
 
-export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={camps}
-        renderItem={({ item }) => (
-          <TouchableOpacity style={{ flex: 1 }} onPress={() => alert(item.name)}>
-            <View style={{ alignItems: "center", marginVertical: 10 }}>
-              <Image
-                resizeMode="cover"
-                style={{ width: 150, height: 150 }}
-                source={{ uri: item.imageUrl }}
-              />
-              <Text>{item.name}</Text>
-            </View>
-          </TouchableOpacity>
-        )}
-        keyExtractor={item => item.id}
-        numColumns={2}
-        showsVerticalScrollIndicator={false}
-      />
-    </SafeAreaView>
-  )
+export default class App extends React.Component {
+  state = { camps: [] }
+  componentDidMount() {
+    this.fetchCamps()
+  }
+
+  fetchCamps = () => {
+    this.setState({ camps: CAMPS })
+  }
+  render() {
+    const { camps } = this.state
+    return (
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={camps}
+          renderItem={({ item }) => (
+            <TouchableOpacity style={{ flex: 1 }} onPress={() => alert(item.name)}>
+              <View style={{ alignItems: "center", marginVertical: 10 }}>
+                <Image
+                  resizeMode="cover"
+                  style={{ width: 150, height: 150 }}
+                  source={{ uri: item.imageUrl }}
+                />
+                <Text>{item.name}</Text>
+              </View>
+            </TouchableOpacity>
+          )}
+          keyExtractor={item => item.id}
+          numColumns={2}
+          showsVerticalScrollIndicator={false}
+        />
+      </SafeAreaView>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
