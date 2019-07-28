eecea139280e7f80eade35ba0d31665155945216
fvonhoven <frank@infinite.red>
2019-07-28 10:19:58 -0500

    Make CampListScreen

--- App.js
+++ App.js
@@ -1,19 +1,43 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react"
+import {
+  StyleSheet,
+  View,
+  SafeAreaView,
+  FlatList,
+  TouchableOpacity,
+  Image,
+  Text
+} from "react-native"
+import { camps } from "./fixtures"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={camps}
+        renderItem={({ item }) => (
+          <TouchableOpacity style={{ flex: 1 }} onPress={() => alert(item.name)}>
+            <View style={{ alignItems: "center", marginVertical: 10 }}>
+              <Image
+                resizeMode="cover"
+                style={{ width: 150, height: 150 }}
+                source={{ uri: item.imageUrl }}
+              />
+              <Text>{item.name}</Text>
+            </View>
+          </TouchableOpacity>
+        )}
+        keyExtractor={item => item.id}
+        numColumns={2}
+        showsVerticalScrollIndicator={false}
+      />
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    backgroundColor: "#fff"
+  }
+})
---
+++ fixtures.js
@@ -0,0 +1,33 @@
+const camps = [
+  {
+    name: "By the Lake Camp",
+    imageUrl: "https://frostvalley.org/wp-content/uploads/2014/10/Cabin-Teen-1024x653.jpg",
+    description:
+      "Get to experience a variety of activities throughout the day and week at camp. Your camper’s day begins and ends with all-camp activities, energizers, songs and  free-play. During the day campers can enjoy Hiking on one of our many nature trails; Fishing using rods or nets from our lake; Sports ~ archery (3rd grade and above), soccer, canoeing, basketball, gaga ball, hockey, and many creative outdoor games that our counselors come up with; Art activities ~ pottery, woodworking, felting, or weaving and Pool Time ~ every week we have pool time – Campers (1-2) also receive swim lessons with our Red Cross trained Water  Safety Instructor, in addition to swim time for practice and play.",
+    id: "da8ab82e4d6cab06"
+  },
+  {
+    name: "Into The Woods",
+    imageUrl:
+      "https://frostvalley.org/wp-content/uploads/2018/02/Cabin-37-Exterior-2016-768x512.jpg",
+    description:
+      "We invite you to discover claws, codes and constellations with our exciting Camp Number 1. Following are some topics you will explore during this fun five-day camp. These are only a sample of some of the activities that you will be doing each day. The topics will not necessarily follow this order. However, each of these topics will be included at some point in the camp.",
+    id: "85932996eaab584f"
+  },
+  {
+    name: "Summer Horizons",
+    imageUrl: "https://cdn.firespring.com/images/9dda7352-00e6-40a0-b435-9b2784d1208c.jpg",
+    description:
+      "Move with science in our exciting Camp Number 2. Following are some topics you will explore during this fun five-day camp. These are only a sample of some of the activities that we will be doing each day. The topics will not necessarily follow this order. However, each of these topics will be included at some point in the camp.",
+    id: "f8ef1e9646997807"
+  },
+  {
+    name: "Rolling Thunder",
+    imageUrl: "https://cdn.firespring.com/images/89beadc3-4f0b-49d1-af4e-ae39dd2dec3f.jpg",
+    description:
+      "We invite you to launch your knowledge of rockets in our exciting Camp Number 4. Following are some topics you will explore during this fun five-day camp. These are only a sample of some of the activities that you will be doing each day. The topics will not necessarily follow this order. However, each of these topics will be included at some point in the camp. We will launch rockets and your child will build several types of rockets. Our camp includes everything your child needs during our camp day. However, if you wish to continue your child’s interest in rocket launches outside our camp, you will need to purchase a launch kit at a hobby shop.",
+    id: "114ca9b270633a36"
+  }
+]
+
+export { camps }
